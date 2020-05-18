72508190748ea86cdff00bfb18d793d045b3c2d0
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-05-18 11:27:15 +0530

    Added new methods of versioning

--- src/main/java/com/springboot/rest/webservices/restwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/springboot/rest/webservices/restwebservices/versioning/PersonVersioningController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 	
+	//METHOD_1
 	@GetMapping("/v1")
 	public PersonV1 personV1() {
 		return new PersonV1("Vatsal Bhatnagar");
@@ -16,5 +17,26 @@ public class PersonVersioningController {
 		return new PersonV2(new Name("Vatsal", "Bhatnagar"));
 	}
 	
+	//METHOD_2(using request parameter)
+	@GetMapping(value = "/person/params", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Vatsal Bhatnagar");
+	}
+	
+	@GetMapping(value = "/person/params", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Vatsal", "Bhatnagar"));
+	}
+	
+	//METHOD_3(using header)
+	@GetMapping(value = "/person/header", headers  = "API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Vatsal Bhatnagar");
+	}
+	
+	@GetMapping(value = "/person/header", headers  = "API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Vatsal", "Bhatnagar"));
+	}
 
 }
