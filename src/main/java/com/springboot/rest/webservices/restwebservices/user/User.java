ea84553fbbd27ecd5aa1f9aa1e521baa0f2b6631
vatsal <vatsal@vatsal-VivoBook>
2020-05-21 11:39:58 +0530

    Added support for JPA

--- src/main/java/com/springboot/rest/webservices/restwebservices/user/User.java
+++ src/main/java/com/springboot/rest/webservices/restwebservices/user/User.java
@@ -2,6 +2,9 @@ package com.springboot.rest.webservices.restwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -9,8 +12,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "Details about user")
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2 , message = "name should have atleast 2 characters")
